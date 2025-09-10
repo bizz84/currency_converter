adba541712824e842f3a928052ed3f9c7cbde115
Andrea Bizzotto <codewithandrea@gmail.com>
2025-09-10 12:10:04 +0200

    Update analysis options, dependencies, and add Dio provider
    - Cleaned up analysis_options.yaml by removing comments and adding a formatter configuration.
    - Updated pubspec.yaml to include riverpod, flutter_riverpod, and riverpod_annotation dependencies.
    - Added dio_provider.dart and its generated file for managing Dio instances with Riverpod, including a logger interceptor for debug mode.
    - Updated pubspec.lock with new dependencies and their respective versions.

---
+++ lib/src/utils/dio_provider.dart
@@ -0,0 +1,17 @@
+import 'package:currency_converter/src/utils/logger_dio_interceptor.dart';
+import 'package:dio/dio.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:flutter/foundation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'dio_provider.g.dart';
+
+@riverpod
+Dio dio(Ref ref) {
+  final dio = Dio();
+  if (kDebugMode) {
+    dio.interceptors.add(LoggerDioInterceptor());
+  }
+  //dio.addSentry();
+  return dio;
+}
