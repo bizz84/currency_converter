e7d5955897d2acaf5eb3504ab52a932b3d712fa2
Andrea Bizzotto <codewithandrea@gmail.com>
2025-11-13 11:13:10 +0100

    chore: update ai_toolkit submodule to latest

--- lib/src/utils/package_info_provider.dart
+++ lib/src/utils/package_info_provider.dart
@@ -1,5 +1,4 @@
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'package_info_provider.g.dart';
